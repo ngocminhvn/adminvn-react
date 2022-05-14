eccddd87ceafc0a5e09403a2d75fe433cc53f2b2
Trinh Ngoc Minh <90078397+ngocminhvn@users.noreply.github.com>
2022-05-15 06:26:03 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Switch, Route } from "react-router-dom";
 
 import Home from "./page/Home";
 import About from "./page/About";
